b1cda8b5d43425dec9cf15d462e97df066ba1de1
xushuai@thunder.com.cn <wangfei88>
2017-10-18 15:42:43 +0800

    Revert "commit"
    This reverts commit 5ec37ad651b2ce75695eae1758a3a4d1ae2c788b.
    
    # Conflicts:
    #	info.php

--- info.php
+++ info.php
@@ -5,9 +5,10 @@ echo 'forenoon';
 echo 'this is comment module';
 echo 'test';
 echo 'to do something...';
-
-echo 'afternoon';
-echo 'test';
-echo 'test';
-echo 'test';
-echo 'aaaa';
+echo 'bbb';
+echo 'bbb';
+echo 'bbb';
+echo 'bbb';
+echo 'bbb';
+echo 'bbb';
+echo 'bbb';
